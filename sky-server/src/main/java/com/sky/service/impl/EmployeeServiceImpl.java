d41507281195085b4ba4e38d509960980cfa35d5
Robin_ <3045351695@qq.com>
2025-01-21 10:57:35 +0800

    启用禁用员工账号

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -111,4 +111,17 @@ public class EmployeeServiceImpl implements EmployeeService {
         return pageResult;
     }
 
+
+    public void startOrStop(Integer status, Long id) {
+        //update employee set status=? where id=?
+//         Employee employee = new Employee();
+//         employee.setStatus(status);
+//         employee.setId(id);
+        Employee employee = Employee.builder()
+                .status(status)
+                .id(id)
+                .build();
+        employeeMapper.update(employee);
+    }
+
 }
